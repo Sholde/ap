7ffc904c3a15df73c0ebe01b7bc29a19bff32129
Sholde <exshode@gmail.com>
2021-01-06 18:08:54 +0100

    separate 1 array and 2 arrays benchmarck

--- TD_mem/exec.sh
+++ TD_mem/exec.sh
@@ -1,38 +1,57 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
-    echo "Usage: $0 core_id"
+# Check arguments
+
+if [ $# -ne 5 ] ; then
+    echo "Usage: $0 core_id l1_size l2_size l3_size ram_size"
     exit 1
 fi
 
 core_id=$1
+l1_size=$2
+l2_size=$3
+l3_size=$4
+ram_size=$5
 
-directories="copy dotprod load ntstore reduc store"
+# 1 array
 
-for directory in $directories
-do
+directories="load ntstore reduc store"
+for directory in $directories ; do 
     cd $directory
     echo "TEST: $directory"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 24 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L1.dat"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L2.dat"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 2000 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L3.dat"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 2000000 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_DRAM.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $l1_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_L1.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $l2_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_L2.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $l3_size* 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_L3.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $ram_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_DRAM.dat"
     cd ..
 done
 
-# pc and memcpy directories
-cd memcpy
-echo "TEST: memcpy"
-taskset -c $core_id ./memcpy $(( 24 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L1.dat
-taskset -c $core_id ./memcpy $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L2.dat
-taskset -c $core_id ./memcpy $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L3.dat
-taskset -c $core_id ./memcpy $(( 2000000 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_DRAM.dat
-cd ..
-
 cd pc
 echo "TEST: pc"
-taskset -c $core_id ./pc $(( 24 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L1.dat
-taskset -c $core_id ./pc $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L2.dat
-taskset -c $core_id ./pc $(( 2000 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L3.dat
-taskset -c $core_id ./pc $(( 2000000 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_DRAM.dat
+taskset -c $core_id ./pc $(( $l1_size * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L1.dat
+taskset -c $core_id ./pc $(( $l2_size * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L2.dat
+taskset -c $core_id ./pc $(( $l3_size * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L3.dat
+taskset -c $core_id ./pc $(( $ram_size * 2**10 )) 1000 | cut -d';' -f1,9 > pc_DRAM.dat
 cd ..
+
+# 2 arrays
+
+directories="copy dotprod"
+for directory in $directories ; do 
+    cd $directory
+    echo "TEST: $directory"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $l1_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_L1.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $l2_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_L2.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $l3_size* 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_L3.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( $ram_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > $directory"_DRAM.dat"
+    cd ..
+done
+
+cd memcpy
+echo "TEST: memcpy"
+taskset -c $core_id ./memcpy $(( $l1_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > memcpy_L1.dat
+taskset -c $core_id ./memcpy $(( $l2_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > memcpy_L2.dat
+taskset -c $core_id ./memcpy $(( $l3_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > memcpy_L3.dat
+taskset -c $core_id ./memcpy $(( $ram_size * 2**10 / 2 )) 1000 | cut -d';' -f1,9 > memcpy_DRAM.dat
+cd ..
+
