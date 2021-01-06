0d1b5f0523a6a5317889ba6690569f192c19af38
Sholde <exshode@gmail.com>
2021-01-06 13:38:18 +0100

    change chache and add dram

--- TD_mem/exec.sh
+++ TD_mem/exec.sh
@@ -13,23 +13,26 @@ for directory in $directories
 do
     cd $directory
     echo "TEST: $directory"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 32 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L1.dat"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 288 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L2.dat"
-    taskset -c $core_id ./$directory"_SSE_AVX" $(( 2368 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L3.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( 24 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L1.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L2.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( 2000 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_L3.dat"
+    taskset -c $core_id ./$directory"_SSE_AVX" $(( 2000000 * 2**10 )) 1000 | cut -d';' -f1,9 > $directory"_DRAM.dat"
     cd ..
 done
 
 # pc and memcpy directories
 cd memcpy
 echo "TEST: memcpy"
-taskset -c $core_id ./memcpy $(( 32 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L1.dat
-taskset -c $core_id ./memcpy $(( 288 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L2.dat
-taskset -c $core_id ./memcpy $(( 2368 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L3.dat
+taskset -c $core_id ./memcpy $(( 24 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L1.dat
+taskset -c $core_id ./memcpy $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L2.dat
+taskset -c $core_id ./memcpy $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_L3.dat
+taskset -c $core_id ./memcpy $(( 2000000 * 2**10 )) 1000 | cut -d';' -f1,9 > memcpy_DRAM.dat
 cd ..
 
 cd pc
 echo "TEST: pc"
-taskset -c $core_id ./pc $(( 32 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L1.dat
-taskset -c $core_id ./pc $(( 288 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L2.dat
-taskset -c $core_id ./pc $(( 2368 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L3.dat
+taskset -c $core_id ./pc $(( 24 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L1.dat
+taskset -c $core_id ./pc $(( 200 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L2.dat
+taskset -c $core_id ./pc $(( 2000 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_L3.dat
+taskset -c $core_id ./pc $(( 2000000 * 2**10 )) 1000 | cut -d';' -f1,9 > pc_DRAM.dat
 cd ..
