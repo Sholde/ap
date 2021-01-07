9fbd4df8fdaf01fd8e4574b1f175de58b405c2f5
Sholde <exshode@gmail.com>
2021-01-07 18:23:39 +0100

    formating

--- TD_mem/compile.sh
+++ TD_mem/compile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+# Compile all benchmark
 directories="copy dotprod load memcpy ntstore pc reduc store"
-
-for directory in $directories
-do
+for directory in $directories ; do
     make -C $directory
 done
